037d954f4b9a7204cb6a42f57f43b0d55d06ee8a
ketvint3 <kethelenvitoria230106@gmail.com>
2023-05-16 17:59:53 -0300

    teste

---
+++ src/main/java/com/Desafio/Final/Diamond/DiamondApplication.java
@@ -0,0 +1,13 @@
+package com.Desafio.Final.Diamond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiamondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiamondApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Desafio/Final/Diamond/DiamondApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Desafio.Final.Diamond;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiamondApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
