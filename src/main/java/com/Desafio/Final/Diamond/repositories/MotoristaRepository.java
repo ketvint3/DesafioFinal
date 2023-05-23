d88dcef01c6a81380bc9fb83cfe361923c674040
athanazio <danielathanazio@gmail.com>
2023-05-23 17:39:54 -0300

    email

--- src/main/java/com/Desafio/Final/Diamond/repositories/MotoristaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Desafio.Final.Diamond.repositories;
-
-import com.Desafio.Final.Diamond.models.MotoristaModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MotoristaRepository extends JpaRepository<MotoristaModel,Integer> {
-}
--- src/main/java/com/Desafio/Final/Diamond/repositories/ViagemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Desafio.Final.Diamond.repositories;
-
-import com.Desafio.Final.Diamond.models.ViagemModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ViagemRepository extends JpaRepository<ViagemModel, Integer> {
-}
