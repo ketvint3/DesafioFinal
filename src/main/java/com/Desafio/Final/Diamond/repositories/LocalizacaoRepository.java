e4a997b4384356d62abcc5daef8bc8feb7eb4735
marilu <125263865+marilupaiva1210@users.noreply.github.com>
2023-05-22 21:02:05 -0300

    Delete LocalizacaoRepository.java

--- src/main/java/com/Desafio/Final/Diamond/repositories/LocalizacaoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Desafio.Final.Diamond.repositories;
-
-import com.Desafio.Final.Diamond.models.LocalizacaoModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocalizacaoRepository extends JpaRepository<LocalizacaoModel, Integer> {
-}
